ae44f9da7467f07686a1e231b912e8188c169812
zangzhan <zangzhan@weanimals.net>
2017-06-23 19:04:23 +0800

    1.0.5

---
+++ test/test.js
@@ -0,0 +1,16 @@
+import assert from 'assert';
+
+describe('Your tests go here!', function() {
+    
+    beforeEach(function(){
+        
+    });
+
+    it('test case 1', function() {
+        
+    });
+
+    it('test case 2', function() {
+        
+    });
+});
